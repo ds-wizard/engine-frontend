8080699170e1454c393368669eb3233f807e8672
Jan Slifka <jan.slifka@gmail.com>
2017-10-07 18:19:36 +0200

    Add views for KM editor

--- src/elm/KnowledgeModels/Editor/View.elm
+++ src/elm/KnowledgeModels/Editor/View.elm
@@ -1,22 +1,111 @@
 module KnowledgeModels.Editor.View exposing (..)
 
 import Common.Html exposing (pageHeader)
-import Html exposing (Html, a, button, div, i, input, label, li, option, select, text, textarea, ul)
+import Html exposing (Html, a, button, div, h3, i, input, label, li, option, select, text, textarea, ul)
 import Html.Attributes exposing (class, href, rows, type_)
 import Msgs exposing (Msg)
+import String exposing (toLower)
 
 
 view : Html Msg
 view =
-    div []
-        [ pageHeader "Knowledge Model Editor" []
-        , breadcrumbs [ "My Knowledge Model", "Chapter 1", "Question 3" ]
+    viewKnowledgeModel
+
+
+viewKnowledgeModel : Html Msg
+viewKnowledgeModel =
+    div [ class editorClass ]
+        [ editorHeader
+        , breadcrumbs [ "My Knowledge Model" ]
+        , editorTitle "Knowledge Model"
         , inputText "Name"
-        , inputTextarea "Text"
-        , inputSelect "Type" [ "Answers", "Text" ]
-        , inputChildren "Answers" [ "Skip", "Explore" ]
+        , inputChildren "Chapter" [ "Design of experiment", "Data design and planning" ]
         , formActions
         ]
+
+
+viewChapter : Html Msg
+viewChapter =
+    div [ class editorClass ]
+        [ editorHeader
+        , breadcrumbs [ "My Knowledge Model", "Chapter 1" ]
+        , editorTitle "Chapter"
+        , inputText "Title"
+        , inputTextarea "Text"
+        , inputChildren "Question" [ "Will you be using new types of data?", "How will you be storin metadata?" ]
+        , formActions
+        ]
+
+
+viewQuestion : Html Msg
+viewQuestion =
+    div [ class editorClass ]
+        [ editorHeader
+        , breadcrumbs [ "My Knowledge Model", "Chapter 1", "Question 1" ]
+        , editorTitle "Question"
+        , inputText "Title"
+        , inputSelect "Type" [ "Answer", "Text" ]
+        , inputTextarea "Text"
+        , inputChildren "Answer" [ "Skip", "Explore" ]
+        , inputChildren "Reference" [ "Reference 1", "Reference 2" ]
+        , inputChildren "Expert" [ "Expert 1", "Expert 2" ]
+        , formActions
+        ]
+
+
+viewAnswer : Html Msg
+viewAnswer =
+    div [ class editorClass ]
+        [ editorHeader
+        , breadcrumbs [ "My Knowledge Model", "Chapter 1", "Question 1", "Answer 1" ]
+        , editorTitle "Answer"
+        , inputText "Label"
+        , inputTextarea "Advice"
+        , inputChildren "Follow-up question" [ "Did you consider how to monitor data integrity?", "Do all datasets you work with have a license?" ]
+        , formActions
+        ]
+
+
+viewExpert : Html Msg
+viewExpert =
+    div [ class editorClass ]
+        [ editorHeader
+        , breadcrumbs [ "My Knowledge Model", "Chapter 1", "Question 1", "Expert 1" ]
+        , editorTitle "Expert"
+        , inputText "Name"
+        , inputText "Email"
+        , formActions
+        ]
+
+
+viewReference : Html Msg
+viewReference =
+    div [ class editorClass ]
+        [ editorHeader
+        , breadcrumbs [ "My Knowledge Model", "Chapter 1", "Question 1", "Reference 1" ]
+        , editorTitle "Reference"
+        , inputText "Chapter"
+        , formActions
+        ]
+
+
+
+-- Common
+
+
+editorHeader : Html Msg
+editorHeader =
+    pageHeader "Knowledge Model Editor" []
+
+
+editorTitle : String -> Html Msg
+editorTitle title =
+    h3 [] [ text title ]
+
+
+editorClass : String
+editorClass =
+    "knowledge-model-editor col-xs-12 col-lg-10 col-lg-offset-1"
 
 
 
@@ -72,13 +161,13 @@ inputSelectOption labelText =
 
 
 inputChildren : String -> List String -> Html Msg
-inputChildren labelText children =
+inputChildren childName children =
     div [ class "form-group" ]
-        [ label [ class "control-label" ] [ text labelText ]
+        [ label [ class "control-label" ] [ text (childName ++ "s") ]
         , div [] (List.map inputChildrenChild children)
         , a [ class "link-with-icon", href "#" ]
             [ i [ class "fa fa-plus" ] []
-            , text "Add answer"
+            , text ("Add " ++ toLower childName)
             ]
         ]
 
