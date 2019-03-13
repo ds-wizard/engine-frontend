0418c18ef4ffc38126ac87aa13e17bd7b4f3f42b
Jan Slifka <jan.slifka@gmail.com>
2019-03-13 13:24:37 +0100

    Fix font-awesome in customized design

--- nginx/start.sh
+++ nginx/start.sh
@@ -3,6 +3,7 @@ sed -i -e 's@http://localhost:3000@'"$API_URL"'@g' /usr/share/nginx/html/*.js
 
 if [[ -f /customizations/variables.scss ]]; then
     cat /customizations/variables.scss > /src/scss/_variables.new.scss
+    echo '$fa-font-path: "";' >> /src/scss/_variables.new.scss
     cat /src/scss/_variables.scss >> /src/scss/_variables.new.scss
     rm /src/scss/_variables.scss
     mv /src/scss/_variables.new.scss /src/scss/_variables.scss
