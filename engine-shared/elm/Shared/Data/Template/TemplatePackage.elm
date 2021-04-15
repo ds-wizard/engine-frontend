ea437e14f885cb434ac0a14a41e67285c7816bc7
Jan Slifka <jan.slifka@gmail.com>
2021-04-15 17:08:44 +0200

    [DSW-1068] Display usable knowledge models on template detail

--- engine-shared/elm/Shared/Data/Template/TemplatePackage.elm
+++ engine-shared/elm/Shared/Data/Template/TemplatePackage.elm
@@ -1,12 +1,14 @@
 module Shared.Data.Template.TemplatePackage exposing
     ( TemplatePackage
     , compare
+    , compareById
     , decoder
     , toFormRichOption
     )
 
 import Json.Decode as D exposing (Decoder)
 import Json.Decode.Pipeline as D
+import Version
 
 
 type alias TemplatePackage =
@@ -47,6 +49,38 @@ compare mbRecommendedId tp1 tp2 =
 
     else
         Basics.compare tp1.name tp2.name
+
+
+compareById : TemplatePackage -> TemplatePackage -> Order
+compareById tp1 tp2 =
+    let
+        split tp =
+            case String.split ":" tp of
+                orgId :: pkgId :: version :: [] ->
+                    ( orgId, pkgId, version )
+
+                _ ->
+                    ( "", "", "" )
+
+        ( tp1orgId, tp1PkgId, tp1version ) =
+            split tp1.id
+
+        ( tp2orgId, tp2PkgId, tp2version ) =
+            split tp2.id
+    in
+    if tp1orgId /= tp2orgId then
+        Basics.compare tp1orgId tp2orgId
+
+    else if tp1PkgId /= tp2PkgId then
+        Basics.compare tp1PkgId tp2PkgId
+
+    else
+        case ( Version.fromString tp1version, Version.fromString tp2version ) of
+            ( Just version1, Just version2 ) ->
+                Version.compare version1 version2
+
+            _ ->
+                EQ
 
 
 matchId : Maybe String -> TemplatePackage -> Bool
