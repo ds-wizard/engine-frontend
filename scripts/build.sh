3d6432be090708483adcdf9ba37de1716712d152
Jan Slifka <jan.slifka@gmail.com>
2021-08-23 16:11:53 +0200

    [DSW-1158] Upgrade node version

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,8 @@
 set -e
 
 rm -rf ./dist/engine-$COMPONENT
-NODE_ENV=production ./node_modules/.bin/webpack -p
+NODE_ENV=production ./node_modules/.bin/webpack
 file=`find dist/engine-$COMPONENT -name "*.js"`
-npx uglifyjs $file --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | npx uglifyjs --mangle --output=$file
+./node_modules/.bin/uglifyjs $file --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | ./node_modules/.bin/uglifyjs --mangle --output $file
 
 $(dirname $0)/build_info.sh
