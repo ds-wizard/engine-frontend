3b4d865b44261385b3aabf5de370d8baea672740
Jan Slifka <jan.slifka@gmail.com>
2021-10-22 10:53:50 +0200

    Add DSW style builder

---
+++ style-builder/scripts/init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm -rf ./src
+mkdir src
+cp -r ../node_modules/@fortawesome ./src/~@fortawesome
+cp -r ../node_modules/bootstrap ./src/~bootstrap
+cp -r ../engine-wizard/scss ./src/scss
