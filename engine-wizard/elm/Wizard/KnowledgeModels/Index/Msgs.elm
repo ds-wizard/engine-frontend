1770af9fd2a91e3b02a1001aba48c62de8239ebf
Jan Slifka <jan.slifka@gmail.com>
2023-04-14 10:18:51 +0200

    [DSW-1899] Fix KM export from KM list

--- engine-wizard/elm/Wizard/KnowledgeModels/Index/Msgs.elm
+++ engine-wizard/elm/Wizard/KnowledgeModels/Index/Msgs.elm
@@ -4,6 +4,7 @@ import Shared.Data.Package exposing (Package)
 import Shared.Data.Package.PackagePhase exposing (PackagePhase)
 import Shared.Error.ApiError exposing (ApiError)
 import Wizard.Common.Components.Listing.Msgs as Listing
+import Wizard.Common.FileDownloader as FileDownloader
 
 
 type Msg
@@ -14,3 +15,4 @@ type Msg
     | UpdatePhase Package PackagePhase
     | UpdatePhaseCompleted (Result ApiError ())
     | ExportPackage Package
+    | FileDownloaderMsg FileDownloader.Msg
