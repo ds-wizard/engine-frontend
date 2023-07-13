36fa870a96d568846ce59d6d8068d2340dcc1206
Jan Slifka <jan.slifka@gmail.com>
2023-07-13 12:32:08 +0200

    [DSW-1998] Fix link to DT development from the dashboard

--- engine-wizard/elm/Wizard/Dashboard/Widgets/ImportDocumentTemplateWidget.elm
+++ engine-wizard/elm/Wizard/Dashboard/Widgets/ImportDocumentTemplateWidget.elm
@@ -11,7 +11,7 @@ view : AppState -> Html msg
 view appState =
     WidgetHelpers.ctaWidget appState
         { title = gettext "Import Document Template" appState.locale
-        , text = gettext "Document templates transform answers from a questionnaire into a document. This document can be anything, from PDF to machine-actionable JSON. You can import existing document templates from [DSW Registry](https://registry.ds-wizard.org/templates) or create new ones using [DSW TDK](https://guide.ds-wizard.org/miscellaneous/development/template-development)." appState.locale
+        , text = gettext "Document templates transform answers from a questionnaire into a document. This document can be anything, from PDF to machine-actionable JSON. You can import existing document templates from [DSW Registry](https://registry.ds-wizard.org/templates) or [develop new ones](https://guide.ds-wizard.org/en/latest/more/development/document-templates/index.html)." appState.locale
         , action =
             { route = Routes.documentTemplatesImport Nothing
             , label = gettext "Import" appState.locale
