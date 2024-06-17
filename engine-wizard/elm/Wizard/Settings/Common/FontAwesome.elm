646e7401309c526d12ccddca2467ff708c3a38eb
Jan Slifka <jan.slifka@gmail.com>
2024-06-17 16:58:02 +0200

    Upgrade Font Awesome to 6

--- engine-wizard/elm/Wizard/Settings/Common/FontAwesome.elm
+++ engine-wizard/elm/Wizard/Settings/Common/FontAwesome.elm
@@ -3,4 +3,4 @@ module Wizard.Settings.Common.FontAwesome exposing (fontAwesomeLink)
 
 fontAwesomeLink : String
 fontAwesomeLink =
-    "https://fontawesome.com/v5/search?o=r&m=free"
+    "https://fontawesome.com/v6/search?o=r&m=free"
