47f7efa8d9922cc7988cf608a7d44149ba7078f5
Jan Slifka <jan.slifka@gmail.com>
2024-06-17 15:46:09 +0200

    Fix WebSocket event listeners

--- engine-shared/ports/WebSocket.js
+++ engine-shared/ports/WebSocket.js
@@ -4,12 +4,13 @@ module.exports = function (app) {
     if (app.ports.wsSend) app.ports.wsSend.subscribe(wsSend)
     if (app.ports.wsPing) app.ports.wsPing.subscribe(wsPing)
 
-    var websockets = {}
-    var queues = {}
+    const websockets = {}
+    const queues = {}
+    const eventListeners = {}
 
     function wsOpen(url) {
         if (!websockets[url]) {
-            var ws = new WebSocket(url)
+            const ws = new WebSocket(url)
             websockets[url] = ws
 
             function sendMsg(type, data) {
@@ -18,33 +19,34 @@ module.exports = function (app) {
                 }
             }
 
-            ws.addEventListener('open', function (event) {
-                sendMsg('open', {})
+            eventListeners[url] = {
+                'open': function (event) {
+                    sendMsg('open', {})
 
-                if (queues[url]) {
-                    while (queues[url].length > 0) {
-                        wsSend([url, queues[url].shift()])
+                    if (queues[url]) {
+                        while (queues[url].length > 0) {
+                            wsSend([url, queues[url].shift()])
+                        }
                     }
+                },
+                'message': function (event) {
+                    sendMsg('message', JSON.parse(event.data))
+                },
+                'close': function (event) {
+                    var reconnect = !event.wasClean && websockets[url]
+
+                    if (reconnect) {
+                        delete websockets[url]
+                        setTimeout(function() { wsOpen(url) }, 500)
+                    } else {
+                        sendMsg('close', null)
+                    }
+                },
+                'error': function (event) {
                 }
-            })
+            }
 
-            ws.addEventListener('message', function (event) {
-                sendMsg('message', JSON.parse(event.data))
-            })
-
-            ws.addEventListener('close', function (event) {
-                var reconnect = !event.wasClean && websockets[url]
-
-                if (reconnect) {
-                    delete websockets[url]
-                    setTimeout(function() { wsOpen(url) }, 500)
-                } else {
-                    sendMsg('close', null)
-                }
-            })
-
-            ws.addEventListener('error', function (event) {
-            })
+            bindEventListeners(ws, eventListeners[url])
         }
     }
 
@@ -65,6 +67,10 @@ module.exports = function (app) {
 
     function wsClose(url) {
         if (websockets[url]) {
+            if (eventListeners[url]) {
+                unbindEventListeners(websockets[url], eventListeners[url])
+                delete eventListeners[url]
+            }
             websockets[url].close()
             delete websockets[url]
         }
@@ -79,5 +85,17 @@ module.exports = function (app) {
             queues[url] = []
         }
         queues[url].push(message)
+    }
+
+    function bindEventListeners(ws, eventListeners) {
+        for (const [event, eventListener] of Object.entries(eventListeners)) {
+            ws.addEventListener(event, eventListener)
+        }
+    }
+
+    function unbindEventListeners(ws, eventListeners) {
+        for (const [event, eventListener] of Object.entries(eventListeners)) {
+            ws.removeEventListener(event, eventListener)
+        }
     }
 }
