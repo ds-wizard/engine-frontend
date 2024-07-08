321fbe95ea3c1fdebc3248285879e5bd3c2ecc06
Jan Slifka <jan.slifka@gmail.com>
2024-07-08 10:33:48 +0200

    Add mailer version to About modal in Registry

--- engine-registry/elm/Registry/Components/AboutModal.elm
+++ engine-registry/elm/Registry/Components/AboutModal.elm
@@ -108,14 +108,21 @@ viewAboutModalContent appState serverBuildInfo =
                     [ text swaggerUrl ]
               )
             ]
+
+        viewComponentVersion component =
+            viewBuildInfo appState component.name component []
+
+        componentVersions =
+            List.map viewComponentVersion (List.sortBy .name serverBuildInfo.components)
     in
     div []
-        [ viewBuildInfo appState (gettext "Client" appState.locale) BuildInfo.client []
-        , viewBuildInfo appState (gettext "Server" appState.locale) serverBuildInfo extraServerInfo
-        ]
+        (viewBuildInfo appState (gettext "Client" appState.locale) BuildInfo.client []
+            :: viewBuildInfo appState (gettext "Server" appState.locale) serverBuildInfo extraServerInfo
+            :: componentVersions
+        )
 
 
-viewBuildInfo : AppState -> String -> BuildInfo -> List ( String, Html msg ) -> Html msg
+viewBuildInfo : AppState -> String -> { a | version : String, builtAt : String } -> List ( String, Html msg ) -> Html msg
 viewBuildInfo appState name buildInfo extra =
     let
         viewExtraRow ( title, value ) =
