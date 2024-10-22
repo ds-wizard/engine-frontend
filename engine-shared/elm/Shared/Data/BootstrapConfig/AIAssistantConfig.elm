0218c0ead56063352a3b98839b2fd64a0587618c
Jan Slifka <jan.slifka@gmail.com>
2024-10-22 10:19:28 +0200

    Add AI Assistant config to bootstrap config

---
+++ engine-shared/elm/Shared/Data/BootstrapConfig/AIAssistantConfig.elm
@@ -0,0 +1,24 @@
+module Shared.Data.BootstrapConfig.AIAssistantConfig exposing
+    ( AIAssistantConfig
+    , decoder
+    , default
+    )
+
+import Json.Decode as D exposing (Decoder)
+import Json.Decode.Pipeline as D
+
+
+type alias AIAssistantConfig =
+    { enabled : Bool
+    }
+
+
+default : AIAssistantConfig
+default =
+    { enabled = False }
+
+
+decoder : Decoder AIAssistantConfig
+decoder =
+    D.succeed AIAssistantConfig
+        |> D.required "enabled" D.bool
