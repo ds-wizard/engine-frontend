7ac3ab502c9d530a265c186ab34c42e0c69c8c1c
Jan Slifka <jan.slifka@gmail.com>
2025-01-22 13:40:32 +0100

    Add guide links workflow

---
+++ scripts/create-guide-links.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+
+
+const elmFile = 'engine-wizard/elm/Wizard/Common/GuideLinks.elm'
+const fileContent = fs.readFileSync(elmFile, 'utf8')
+const keyValueRegex = /\( "(.*?)", "(.*?)" \)/g
+const result = {}
+
+let line
+while ((line = keyValueRegex.exec(fileContent)) !== null) {
+    result[line[1]] = line[2]
+}
+
+fs.writeFileSync('guide-links.json', JSON.stringify(result, null, 2))
