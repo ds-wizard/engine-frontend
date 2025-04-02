3ee133d344600ea3dd210e1225e491cb74c58b31
Jan Slifka <jan.slifka@gmail.com>
2025-04-02 10:05:30 +0200

    Fix anonymous user is creating requests to AI Assistant API

--- engine-wizard/elm/Wizard.elm
+++ engine-wizard/elm/Wizard.elm
@@ -3,6 +3,7 @@ module Wizard exposing (main)
 import Browser
 import Browser.Navigation exposing (Key)
 import Json.Decode exposing (Value)
+import Shared.Auth.Session as Session
 import Shared.Data.BootstrapConfig.Admin as Admin
 import Shared.Utils as Taks exposing (dispatch)
 import Shared.Utils.Theme as Theme
@@ -52,7 +53,7 @@ init flags location key =
                                 Cmd.none
 
                     aiAssistantCmd =
-                        if Admin.isEnabled appState.config.admin then
+                        if Admin.isEnabled appState.config.admin && Session.exists appState.session then
                             Taks.dispatch (Wizard.Msgs.AIAssistantMsg AIAssistant.init)
 
                         else
