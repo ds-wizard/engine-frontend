637026ac90ace0db1312204fa4d18e2895694c53
Jan Slifka <jan.slifka@gmail.com>
2025-11-19 12:03:29 +0100

    Add sending token to URL checker

--- app-wizard/elm/Wizard/Api/UrlCheck.elm
+++ app-wizard/elm/Wizard/Api/UrlCheck.elm
@@ -1,6 +1,7 @@
 module Wizard.Api.UrlCheck exposing (postUrlCheck)
 
 import Common.Api.Request as Request exposing (ToMsg)
+import String.Extra as String
 import Wizard.Api.Models.UrlCheckRequest as UrlCheckRequest exposing (UrlCheckRequest)
 import Wizard.Api.Models.UrlCheckResponse as UrlCheckResponse exposing (UrlCheckResponse)
 import Wizard.Data.AppState exposing (AppState)
@@ -11,7 +12,7 @@ postUrlCheck appState body =
     let
         serverInfo =
             { apiUrl = Maybe.withDefault "" appState.urlCheckerUrl
-            , token = Nothing
+            , token = String.toMaybe appState.session.token.token
             }
     in
     Request.post serverInfo "" UrlCheckResponse.decoder (UrlCheckRequest.encode body)
