977f9eed226cf47bbfa9e552deb8be77042f3fbf
Jan Slifka <jan.slifka@gmail.com>
2025-12-09 09:31:45 +0100

    Add show/hide password eye icon to Create user

--- app-wizard/elm/Wizard/Pages/Users/Create/View.elm
+++ app-wizard/elm/Wizard/Pages/Users/Create/View.elm
@@ -1,33 +1,34 @@
 module Wizard.Pages.Users.Create.View exposing (view)
 
-import Common.Components.ActionButton as ActionButton
+import Common.Components.Container as Container
+import Common.Components.Form as Form
 import Common.Components.FormGroup as FormGroup
-import Common.Components.FormResult as FormResult
 import Common.Components.Page as Page
 import Common.Data.Role as Role
 import Common.Utils.Form.FormError exposing (FormError)
 import Form exposing (Form)
 import Gettext exposing (gettext)
 import Html exposing (Html, div)
-import Html.Events exposing (onSubmit)
-import Wizard.Components.FormActions as FormActions
 import Wizard.Data.AppState as AppState exposing (AppState)
 import Wizard.Pages.Users.Common.UserCreateForm exposing (UserCreateForm)
 import Wizard.Pages.Users.Create.Models exposing (Model)
 import Wizard.Pages.Users.Create.Msgs exposing (Msg(..))
-import Wizard.Utils.HtmlAttributesUtils exposing (detailClass)
 import Wizard.Utils.WizardGuideLinks as WizardGuideLinks
 
 
 view : AppState -> Model -> Html Msg
 view appState model =
-    Html.form [ onSubmit (FormMsg Form.Submit), detailClass "Users__Create" ]
+    Container.simpleForm
         [ Page.headerWithGuideLink (AppState.toGuideLinkConfig appState WizardGuideLinks.usersCreate) (gettext "Create User" appState.locale)
-        , FormResult.view model.savingUser
-        , formView appState model.form |> Html.map FormMsg
-        , FormActions.viewSubmit appState
-            Cancel
-            (ActionButton.SubmitConfig (gettext "Create" appState.locale) model.savingUser)
+        , Form.viewSimple
+            { formMsg = FormMsg
+            , formResult = model.savingUser
+            , formView = formView appState model.form |> Html.map FormMsg
+            , submitLabel = gettext "Create" appState.locale
+            , cancelMsg = Just Cancel
+            , locale = appState.locale
+            , isMac = appState.navigator.isMac
+            }
         ]
 
 
@@ -39,5 +40,5 @@ formView appState form =
         , FormGroup.input appState.locale form "lastName" <| gettext "Last name" appState.locale
         , FormGroup.inputWithTypehints appState.config.organization.affiliations appState.locale form "affiliation" <| gettext "Affiliation" appState.locale
         , FormGroup.select appState.locale (Role.options appState) form "role" <| gettext "Role" appState.locale
-        , FormGroup.passwordWithStrength appState.locale form "password" <| gettext "Password" appState.locale
+        , FormGroup.secret appState.locale form "password" <| gettext "Password" appState.locale
         ]
