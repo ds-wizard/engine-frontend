d2ae0e7a73d931e0d842f80c3ce19f02b47f64ab
Jan Slifka <jan.slifka@gmail.com>
2026-02-26 09:01:00 +0100

    Fix allowed packages in document template editor

--- app-wizard/elm/Wizard/Api/Models/DocumentTemplate/DocumentTemplateAllowedPackage.elm
+++ app-wizard/elm/Wizard/Api/Models/DocumentTemplate/DocumentTemplateAllowedPackage.elm
@@ -17,20 +17,20 @@ import Json.Encode.Extra as E
 
 
 type alias DocumentTemplateAllowedPackage =
-    { kmId : Maybe String
-    , maxVersion : Maybe String
+    { orgId : Maybe String
+    , kmId : Maybe String
     , minVersion : Maybe String
-    , orgId : Maybe String
+    , maxVersion : Maybe String
     }
 
 
 decoder : Decoder DocumentTemplateAllowedPackage
 decoder =
     D.succeed DocumentTemplateAllowedPackage
-        |> D.required "kmId" (D.maybe D.string)
-        |> D.required "maxVersion" (D.maybe D.string)
-        |> D.required "minVersion" (D.maybe D.string)
         |> D.required "orgId" (D.maybe D.string)
+        |> D.required "kmId" (D.maybe D.string)
+        |> D.required "minVersion" (D.maybe D.string)
+        |> D.required "maxVersion" (D.maybe D.string)
 
 
 encode : DocumentTemplateAllowedPackage -> E.Value
